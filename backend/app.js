04e80e936715883615c5aae64830375730cd9a51
Trang Do <quynhtrangdo94@gmail.com>
2019-07-24 18:07:07 -0400

    Created first api route with mock data

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {
-  console.log("first middleware");
-  next();
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    { 
+      id: "ashdgr348hk",
+      title: "First server post",
+      content: "this is from server"
+    },
+    { 
+      id: "jhwg237648vj",
+      title: "Second server post",
+      content: "this is from server too"
+    }
+  ]
+  res.status(200).json({
+    message: "Posts fetched successfully",
+    posts: posts
+  })
 })
 
 module.exports = app;
