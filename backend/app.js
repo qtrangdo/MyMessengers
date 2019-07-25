b13dc98d90d8ac469398ea756703d42fd63695ba
Trang Do <quynhtrangdo94@gmail.com>
2019-07-25 10:41:39 -0400

    Fetched and stored data to DB

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 const Post = require('./models/post');
 
-mongoose.connect("mongodb+srv://user:YuRSoRQL2jXiyTjZ@cluster0-faqcb.mongodb.net/test?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://user:YuRSoRQL2jXiyTjZ@cluster0-faqcb.mongodb.net/node-angular?retryWrites=true&w=majority", { useNewUrlParser: true })
   .then(() => console.log("Connected to DB!"))
   .catch(err => console.log("Connection failed..."))
 
@@ -16,27 +16,24 @@ app.use(cors());
 app.post('/api/posts', (req, res) => {
   const { title, content } = req.body
   const post = new Post({ title, content })
-  console.log(post)
-  res.status(201).send();
+  post.save();
+  res.status(201).json({
+    message: "Post added successfully"
+  })
 })
 
-app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: "ashdgr348hk",
-      title: "First server post",
-      content: "this is from server"
-    },
-    {
-      id: "jhwg237648vj",
-      title: "Second server post",
-      content: "this is from server too"
-    }
-  ]
-  res.status(200).json({
-    message: "Posts fetched successfully",
-    posts: posts
-  })
+app.get('/api/posts', async (req, res, next) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json({
+      message: "Posts fetched successfully",
+      posts: posts
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: "Error getting data"
+    })
+  }
 })
 
 module.exports = app;
