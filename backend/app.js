072bb5f979394babedd147942e444c0f20005e12
Trang Do <quynhtrangdo94@gmail.com>
2019-07-25 10:14:32 -0400

    Connected to DB, fixed minor styling

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,32 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+const Post = require('./models/post');
+
+mongoose.connect("mongodb+srv://user:YuRSoRQL2jXiyTjZ@cluster0-faqcb.mongodb.net/test?retryWrites=true&w=majority")
+  .then(() => console.log("Connected to DB!"))
+  .catch(err => console.log("Connection failed..."))
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/api/posts', (req,res) => {
-  console.log(req.body)
+app.post('/api/posts', (req, res) => {
+  const { title, content } = req.body
+  const post = new Post({ title, content })
+  console.log(post)
   res.status(201).send();
 })
 
 app.get('/api/posts', (req, res, next) => {
   const posts = [
-    { 
+    {
       id: "ashdgr348hk",
       title: "First server post",
       content: "this is from server"
     },
-    { 
+    {
       id: "jhwg237648vj",
       title: "Second server post",
       content: "this is from server too"
