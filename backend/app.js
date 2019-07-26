8b4e0ee22f782157cd044c0675f4de31706ba0f8
Trang Do <quynhtrangdo94@gmail.com>
2019-07-26 11:16:46 -0400

    Fetched and displayed img from front end

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require("cors");
@@ -13,6 +14,8 @@ mongoose.connect("mongodb+srv://user:YuRSoRQL2jXiyTjZ@cluster0-faqcb.mongodb.net
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use('/images', express.static(path.join("backend/images")));
 
 app.use('/api/posts', postsController)
 
